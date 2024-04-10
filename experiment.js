12d054b2b8c3ba0376998e986a397c62f7ec2d3a
ShatakshiSaxena125 <126514778+ShatakshiSaxena125@users.noreply.github.com>
2024-04-10 19:41:29 +0530

    Add files via upload

--- experiment.js
+++ experiment.js
@@ -1,7 +1,7 @@
 class Experiment {
   // Group Details
-  static rollNos = '10983437,10983743'
-  static names = 'The Tutors(Akhtar Banga, Phul Tekchand)'
+  static rollNos = '102117163, 102117165'
+  static names = 'The Invincibles(Cheshta Arora, Shatakshi Saxena)'
 
   canvasSel = '#myCanvas'
 
