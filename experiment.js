7ee8e1ce753e6a6892e2eca15082cae120029344
ShatakshiSaxena125 <126514778+ShatakshiSaxena125@users.noreply.github.com>
2024-04-10 19:43:22 +0530

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -1,7 +1,7 @@
 class Experiment {
   // Group Details
   static rollNos = '102117163, 102117165'
-  static names = 'The Invincibles(Cheshta Arora, Shatakshi Saxena)'
+  static names = 'The Invincibles(Cheshta Arora, Shatakshi Saxena)'   //Upated details
 
   canvasSel = '#myCanvas'
 
